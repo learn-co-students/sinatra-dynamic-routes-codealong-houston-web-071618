5016556bc23b5d0c1df759f75cd0f88fdc48f0cc
Ray <ray.rodz.88@gmail.com>
2018-08-10 09:25:04 -0500

    Done.

--- app.rb
+++ app.rb
@@ -21,9 +21,10 @@ get "/goodbye/:name" do
 end 
 
 get "/multiply/:num1/:num2" do 
-  @product = params[:num1].to_i * params[:num2].to_i
-    "#{@product}"
+  @num1 = params[:num1]
+  @num2 = params[:num2]
 
+  @num1 * @num2
 end 
    
 end
